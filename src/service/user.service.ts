5c723752d2acae6ecb8868c8f633a123e280b351
Felipe Souza <fesouza2345@gmail.com>
2023-06-12 22:36:12 -0300

    .

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -41,7 +41,7 @@ class UserService {
     context.res.cookie("accessToken", token, {
       maxAge: 60 * 60 * 1000, // 1 hour
       httpOnly: false,
-      domain: process.env["CLIENT_URL"] as string,
+      domain: "stock-control-web",
       path: "/",
       sameSite: "none",
       secure: true,
